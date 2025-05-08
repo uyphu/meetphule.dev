f2cd871a6d5f8ccdc48c9e3aa42e1968f25d9f92
Phu Le <uyphu@yahoo.com>
2025-05-07 19:36:44 -0700

    [MPL-31] Contact form

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,11 +1,13 @@
 // src/pages/Contact.tsx
 import React from "react";
 import ContactInfo from "../components/ContactInfo";
+import ContactForm from "../components/ContactForm";
 
 const Contact: React.FC = () => {
   return (
     <main className="bg-white dark:bg-gray-900">
       <ContactInfo />
+      <ContactForm/>
     </main>
   );
 };
